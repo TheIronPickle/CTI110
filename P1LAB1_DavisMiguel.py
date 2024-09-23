b94993d9b9df55757ac1b560928bc7423581c88d
TheIronPickle <miguel.davis87@hotmail.com>
2024-09-22 20:02:03 -0400

    Add files via upload
    First submission

---
+++ P1LAB1_DavisMiguel.py
@@ -0,0 +1,8 @@
+# Miguel Davis
+# 9/22/24
+# P1LAB1
+# This program will obtain name input from the user and display output along with a welcome message
+
+firstname = input("Enter your first name: ")
+lastname = input("Enter your last name: ")
+print("Hello,", firstname, lastname,"! Welcome to CTI-110")
